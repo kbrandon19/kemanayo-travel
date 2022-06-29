96948be03fd6208c1308a3780f15233bd216a2b9
kbrandon19 <48884692+kbrandon19@users.noreply.github.com>
2022-06-28 22:22:06 -0500

    started mobile nav and included icons

--- src/Components/Navigation/Navigation.jsx
+++ src/Components/Navigation/Navigation.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCompass,faSuitcase, faTicket, faCar, faCircleUser } from '@fortawesome/free-solid-svg-icons'
+
 import './Navigation.css'
 
 function Navigation() {
@@ -10,7 +12,29 @@ function Navigation() {
   <>
   <header>
     <nav className="nav">
-      <div className="mobileNav"></div>
+      <div className="mobileNav">
+        <div className="notification"></div>
+        <div className="mobile-links">
+         <div className="link-icon" id="explore">
+          <p>Explore</p>
+          <FontAwesomeIcon icon={faCompass} />
+         </div>
+         <div className="link-icon" id="trips">
+         <p>Trips</p>
+         <FontAwesomeIcon icon={faSuitcase} /></div>
+         <div className="link-icon" id="events">
+         <p>Events</p>
+         <FontAwesomeIcon icon={faTicket} />
+         </div>
+         <div className="link-icon" id="rentals">
+         <p>Rentals</p>
+         <FontAwesomeIcon icon={faCar} />
+         </div>
+         <div className="link-icon" id="login">
+         <p>Login</p>
+         <FontAwesomeIcon icon={faCircleUser} /></div>
+        </div>
+      </div>
     </nav>
   </header>
   </>
