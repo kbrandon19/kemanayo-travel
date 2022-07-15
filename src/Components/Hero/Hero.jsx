3a05311a9a1f269b3edc709f78c7abd3266a652f
kbrandon19 <48884692+kbrandon19@users.noreply.github.com>
2022-07-15 18:23:02 -0500

    completed search bar and hero content layout

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons'
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 import './Hero.css'
 
 function Hero() {
@@ -11,14 +11,21 @@ function Hero() {
           <div className="hero-content">
             <h1>Beragam Tujuan Menarik</h1>
             <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit dicta.
+            Dengan kemanayo, tenukan momen spesial dan berkesan di setiap perjalananmu
             </p>
             <div className="search-bar">
-              <input type="text" name="location" id="location-name" placeholder="Trips, places and activities"/>
-              <FontAwesomeIcon icon={faMagnifyingGlass}/>
+              <form action="#">
+                <input type="text" name="location" id="location-name" placeholder="Trips, places and activities"/>
+                <div className="search-icon">
+<FontAwesomeIcon icon={faMagnifyingGlass} />
+                </div>
+              </form>
+            
             </div>
           </div>
-          <div className="location-carousel"><h1>Swiper JS</h1></div>
+          <div className="location-carousel">
+          <h1>Swiper JS</h1>
+          </div>
         </div>
       </main>
     </>
