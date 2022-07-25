4fa649d86e255f6fe181a24eb6d5b56a2d3ccfdb
kbrandon19 <48884692+kbrandon19@users.noreply.github.com>
2022-07-25 11:53:45 -0500

    adjusted pagination colors and size, added more images

--- src/Components/Hero/Carousel.jsx
+++ src/Components/Hero/Carousel.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import {Pagination, Keyboard} from 'swiper';
 import {Swiper,SwiperSlide} from 'swiper/react';
-import 'swiper/css';
+import 'swiper/css/bundle';
 import './Hero.css';
-import 'swiper/css/pagination';
-import 'swiper/css/keyboard';
-
 import Bali from '../../assets/bali.jpg';
 import Yogyakarta from '../../assets/yogyakarta.jpg';
 import Phuket from '../../assets/phuket.jpg';
+import Jatiluwuh from '../../assets/jatiluwuh.jpg';
+import PhiPhi from '../../assets/phiphi.jpg';
 
 
 function Carousel() {
@@ -16,19 +15,22 @@ function Carousel() {
     <>
 <Swiper 
 modules={[Pagination,Keyboard]}
-spaceBetween={20}
+spaceBetween={40}
 slidesPerView={'auto'}
 centeredSlides={true}
+draggable={true}
 pagination={{clickable:true}}
 >
+ 
+    <SwiperSlide>
+    <img class='destination-img' src={PhiPhi} alt="" />
+    </SwiperSlide>
+    <SwiperSlide>
+    <img class='destination-img' src={Jatiluwuh} alt="" />
+    </SwiperSlide>
     <SwiperSlide>
     <img class='destination-img' src={Bali} alt="" />
     </SwiperSlide>
-
-    <SwiperSlide>
-    <img class='destination-img' src={Yogyakarta} alt="" />
-    </SwiperSlide>
-
     <SwiperSlide>
     <img class='destination-img' src={Phuket} alt="" />
     </SwiperSlide>
