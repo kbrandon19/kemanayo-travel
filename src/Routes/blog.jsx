f7851179c68824c1db0574745bfdc27c3d769d2d
kbrandon19 <48884692+kbrandon19@users.noreply.github.com>
2022-08-14 11:42:41 -0500

    continuing styling and design of blog component

--- src/Routes/blog.jsx
+++ src/Routes/blog.jsx
@@ -1,27 +1,37 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable jsx-a11y/heading-has-content */
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
-import Background from '../Components/Background/Background'
-import Navigation from '../Components/Navigation/Navigation'
+// import Background from '../Components/Background/Background'
+import Navigation from '../Components/Navigation/Navigation';
+
 import "../Routes/routes.css"
 
-import PostImage from '../assets/blog.jpg'
+
+
 
 
 function blog() {
   return (
     <>
-      <Background/>
-    <Navigation/>
+      {/* <Background/> */}
+    <Navigation />
      <div className="main-container">
      <div className="blog-container">
        <div className="post-highlight">
        <div className="post-image">
-       <PostImage />
+       
        </div>
-        <div className="post-info">postinfo
-        <h3></h3>
-        <div className="post-author"></div>
+        <div className="post-info">
+        <h1>Places Less Traveled Are The Most Memorable</h1>
+        <div className="post-author">
+          <div className="post-name">
+            <div className="profile-avi"></div>
+            <p>Jason Fletcher</p>
+          </div>
+          <span id="auth-date">29, April 2014</span>
+
+        </div>
         </div>
        </div>
         <div className="blog-posts"></div>
