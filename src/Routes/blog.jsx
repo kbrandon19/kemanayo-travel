f65ef9fc09aa636f173e9e01bfea2020b1161d3c
kbrandon19 <48884692+kbrandon19@users.noreply.github.com>
2022-08-15 12:21:23 -0500

    removed blog posts to reconfig for mobile

--- src/Routes/blog.jsx
+++ src/Routes/blog.jsx
@@ -18,13 +18,15 @@ function blog() {
     <Navigation />
      <div className="main-container">
      <div className="blog-container">
+   
        <div className="post-highlight">
        <div className="post-image">
        
        </div>
         <div className="post-info">
         <h1>Places Less Traveled Are The Most Memorable</h1>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem debitis eveniet fugit? Tenetur, quaerat doloremque. Lorem ipsum dolor sit amet. <a href="#" id="blog-link">Read More</a></p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem debitis eveniet fugit? Tenetur, quaerat doloremque. Lorem ipsum dolor sit amet.</p>
+        <a href="#" id="blog-link">Read More</a>
   
         <div className="post-author">
           <div className="post-name">
@@ -37,10 +39,7 @@ function blog() {
         </div>
        </div>
 
-       <div className="blog-post">
-        <div className="post">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur veritatis dolorum repudiandae! Et distinctio doloribus aut quaerat eum nobis consequatur, ex itaque sit obcaecati assumenda placeat culpa earum omnis natus?</div>
-        <div className="post">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur veritatis dolorum repudiandae! Et distinctio doloribus aut quaerat eum nobis consequatur, ex itaque sit obcaecati assumenda placeat culpa earum omnis natus?</div>
-       </div>
+
      </div>
      </div>
     </>
