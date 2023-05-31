40804ed8c38c17f99af3e39741711e2e5baecde1
Ken <user@192.168.1.9>
2023-05-30 23:19:43 -0500

    adjusted amenities font and icon size, spacing for DesktopNav

--- src/Components/Amenities/Amenities.jsx
+++ src/Components/Amenities/Amenities.jsx
@@ -9,30 +9,30 @@ function Amenities() {
     <>
     <div className="amenities">
       <div className="amen-text">
-        <h2><span>Kemanayo</span> is Your Personal Travel Assistant</h2>
+        <h2><span>Kemanayo</span> is Your Personal <br/>Travel Assistant</h2>
       </div>
       <div className="amen-list">
         <div className='amen-list__label'>
         <div className="icon">
-          <FaConciergeBell color="#6BD0D6" id="icon bell" size={40}/>
+          <FaConciergeBell color="#6BD0D6" id="icon bell" size={30}/>
         </div>
         <p>Liburan gak hanya sekedar wacana</p>
       </div>
       <div className='amen-list__label'>
         <div className="icon">
-          <FaSuitcase color="#6BD0D6" id="icon bell" size={40}/>
+          <FaSuitcase color="#6BD0D6" id="icon bell" size={30}/>
         </div>
         <p>Pelayanan terbaik <br></br>untuk anda</p>
       </div>
       <div className='amen-list__label'>
         <div className="icon">
-          <MdTravelExplore color="#6BD0D6" id="icon bell" size={40}/>
+          <MdTravelExplore color="#6BD0D6" id="icon bell" size={30}/>
         </div>
         <p>Kemudahan akses website dan apps</p>
       </div>
       <div className='amen-list__label'>
         <div className="icon">
-          <MdPayments color="#6BD0D6" id="icon bell" size={40}/>
+          <MdPayments color="#6BD0D6" id="icon bell" size={30}/>
         </div>
         <p>Pembayaran yang cepat dan aman</p>
       </div>
